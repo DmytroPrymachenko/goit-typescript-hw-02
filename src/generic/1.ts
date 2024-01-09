87804fea7d71afc29d63133db5bba26a64159a17
DmytroPrymachenko <144675672+DmytroPrymachenko@users.noreply.github.com>
2024-01-09 11:25:31 +0200

    корегування 1шого та 3тього завдання

--- src/generic/1.ts
+++ src/generic/1.ts
@@ -3,15 +3,16 @@
   Доповніть цю функцію, використовуючи generics, щоб вона повертала правильний тип.
 */
 
-function getPromise<T>(): Promise<T[]> {
+function getPromise<T extends [string , number]>(): Promise<T> {
   return new Promise((resolve) => {
-    resolve(['Text', 50] as T[]);
+    resolve(['Text', 50] as T);
   });
 }
 
-getPromise<string | number>()
-.then((data) => {
-  console.log(data);
+getPromise()
+  .then((data: [string, number]) => {
+    console.log(data);
 });
 
 export {};
+
